98f162ff808042990ed1ec6d4e7b9d4b2778c191
Tom De Vylder <tom@penumbra.be>
2012-04-03 14:58:16 +0200

    Add support for centos 5/6 i386/i686 architectures

--- build.sh
+++ build.sh
@@ -30,8 +30,28 @@ do
   cp nagios-plugin-${PLUGIN_NAME_DEBIAN}_${PLUGIN_VERSION}-${PLUGIN_ITERATION}_all.deb packages/debian/5/
   mv nagios-plugin-${PLUGIN_NAME_DEBIAN}_${PLUGIN_VERSION}-${PLUGIN_ITERATION}_all.deb packages/debian/6/
 
+  # Build RHEL 5/6 i386 package
+  setarch i386 fpm -s dir -t rpm --architecture i386 \
+    -n nagios-plugins-${PLUGIN_NAME_RHEL} \
+    -v ${PLUGIN_VERSION} --iteration ${PLUGIN_ITERATION} \
+    --prefix /usr/lib64/nagios/plugins/ \
+    --description "Nagios Plugin - ${PLUGIN}" \
+    ${PLUGIN} &>/dev/null
+  cp nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.i386.rpm packages/rhel/5/
+  mv nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.i386.rpm packages/rhel/6/
+
+  # Build RHEL 5/6 i686 package
+  setarch i686 fpm -s dir -t rpm --architecture i686 \
+    -n nagios-plugins-${PLUGIN_NAME_RHEL} \
+    -v ${PLUGIN_VERSION} --iteration ${PLUGIN_ITERATION} \
+    --prefix /usr/lib64/nagios/plugins/ \
+    --description "Nagios Plugin - ${PLUGIN}" \
+    ${PLUGIN} &>/dev/null
+  cp nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.i686.rpm packages/rhel/5/
+  mv nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.i686.rpm packages/rhel/6/
+
   # Build RHEL 5/6 x86_64 package
-  fpm -s dir -t rpm --architecture x86_64 \
+  setarch x86_64 fpm -s dir -t rpm --architecture x86_64 \
     -n nagios-plugins-${PLUGIN_NAME_RHEL} \
     -v ${PLUGIN_VERSION} --iteration ${PLUGIN_ITERATION} \
     --prefix /usr/lib64/nagios/plugins/ \
