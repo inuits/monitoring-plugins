4676308bd3fefd601e13e606ef25abb5214f3519
Lander Van den Bulcke <landervdb@inuits.eu>
2018-08-24 15:06:47 +0200

    Fix build.sh underscore handling in Debian package names

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ fi
 
 for PLUGIN in $(grep -viE '^#|^$' build.txt | awk {'print $1'} | sort | uniq )
 do
-  PLUGIN_NAME_DEBIAN=$(echo ${PLUGIN} | sed -e 's/check_/check-/g' | cut -d '.' -f 1)
+  PLUGIN_NAME_DEBIAN=$(echo ${PLUGIN} | sed -e 's/check_/check-/g' | tr '_' '-' | cut -d '.' -f 1)
   PLUGIN_NAME_RHEL=$(echo ${PLUGIN} | sed -e 's/check_//g' | cut -d '.' -f 1)
   PLUGIN_VERSION=$(grep -E "^${PLUGIN}\s" build.txt | awk {'print $2'})
   PLUGIN_ITERATION=$(git rev-list HEAD -- "${PLUGIN}" | wc -l)
