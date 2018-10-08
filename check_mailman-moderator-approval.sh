bed843940a4d3d2ce8652711d78691610f0f24a6
Pavel Pulec <kayn@inuits.eu>
2018-10-08 15:42:37 +0200

    check_mailman-moderator-approval.sh - echo the list of queues waiting for a moderator approval
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- check_mailman-moderator-approval.sh
+++ check_mailman-moderator-approval.sh
@@ -17,6 +17,10 @@ while getopts d:w:c: opts; do
     esac
 done
 
+queues () {
+  pending_queues=$(ls "${data_dir}"/heldmsg-*.pck 2>/dev/null | sed -r '/^$/d;s/.*heldmsg-(.*)-[0-9]+.pck/\1/' | sort -u | tr '\n' ' ')
+}
+
 err=$(ls "${data_dir}" 2>&1 >/dev/null) || {
   echo "UNKNOWN: The content of '${data_dir}' directory can't be listed. Error: ${err}"
   exit 3
@@ -25,10 +29,12 @@ err=$(ls "${data_dir}" 2>&1 >/dev/null) || {
 count=$(ls "${data_dir}"/heldmsg-*.pck 2>/dev/null | wc -l)
 
 if [ "${count}" -ge "${critical}" ]; then
-  echo "CRITICAL: ${count} messages waiting for a moderator approval"
+  queues
+  echo "CRITICAL: ${count} messages waiting for a moderator approval in the queues: ${pending_queues}"
   exit 2
 elif [ "${count}" -ge "${warning}" ]; then
-  echo "WARNING: ${count} messages waiting for a moderator approval"
+  queues
+  echo "WARNING: ${count} messages waiting for a moderator approval in the queues: ${pending_queues}"
   exit 1
 else
   echo "OK: no messages waiting for a moderator approval"
