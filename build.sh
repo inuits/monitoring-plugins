0b27022d7ec7181eacc7f67311bf26d7056e68e3
Hans Broeckx <1036421+hbro@users.noreply.github.com>
2024-09-10 14:21:11 +0200

    Set url as upstream repo in rpm, add rhel8/9 entries, bit of autoformatting, update minor version of script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Script:  build.sh
-## Version: 0.1.4
+## Version: 0.1.5
 ## Date:    2014-01-31
 ## Author:  Tom De Vylder <tomdv@inuits.eu>
 ## Contrib:
@@ -11,18 +11,17 @@ set -e -x
 rm -rf *.deb
 rm -rf *.rpm
 
-if [ -z "${GIT_PREVIOUS_COMMIT}" ] && [ -e last_success ]
-then
+if [ -z "${GIT_PREVIOUS_COMMIT}" ] && [ -e last_success ]; then
   GIT_PREVIOUS_COMMIT=$(cat last_success)
 fi
 
-for PLUGIN in $(grep -viE '^#|^$' build.txt | awk {'print $1'} | sort | uniq )
-do
+for PLUGIN in $(grep -viE '^#|^$' build.txt | awk {'print $1'} | sort | uniq); do
   PLUGIN_NAME_DEBIAN=$(echo ${PLUGIN} | sed -e 's/check_/check-/g' | tr '_' '-' | cut -d '.' -f 1)
   PLUGIN_NAME_RHEL=$(echo ${PLUGIN} | sed -e 's/check_//g' | cut -d '.' -f 1)
   PLUGIN_VERSION=$(grep -E "^${PLUGIN}\s" build.txt | awk {'print $2'})
   PLUGIN_ITERATION=$(git rev-list HEAD -- "${PLUGIN}" | wc -l)
   PLUGIN_EPOCH=$(date +'%s')
+  PLUGIN_URL="https://github.com/inuits/monitoring-plugins"
 
   echo -e "\e[1;34m[\e[00m --- \e[00;32mBuild package: ${PLUGIN}\e[00m --- \e[1;34m]\e[00m"
 
@@ -35,6 +34,7 @@ do
     -v ${PLUGIN_VERSION} --iteration ${PLUGIN_ITERATION} --epoch $PLUGIN_EPOCH \
     --prefix /usr/lib/nagios/plugins \
     --description "Nagios Plugin - ${PLUGIN_NAME_DEBIAN}" \
+    --url "${PLUGIN_URL}" \
     ${PLUGIN} &>/dev/null
   cp nagios-plugin-${PLUGIN_NAME_DEBIAN}_${PLUGIN_VERSION}-${PLUGIN_ITERATION}_all.deb packages/debian/5/
   mv nagios-plugin-${PLUGIN_NAME_DEBIAN}_${PLUGIN_VERSION}-${PLUGIN_ITERATION}_all.deb packages/debian/6/
@@ -45,6 +45,7 @@ do
     -v ${PLUGIN_VERSION} --iteration ${PLUGIN_ITERATION} --epoch $PLUGIN_EPOCH \
     --prefix /usr/lib64/nagios/plugins \
     --description "Nagios Plugin - ${PLUGIN}" \
+    --url "${PLUGIN_URL}" \
     ${PLUGIN} &>/dev/null
   cp nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.i386.rpm packages/rhel/5/
   mv nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.i386.rpm packages/rhel/6/
@@ -55,20 +56,24 @@ do
     -v ${PLUGIN_VERSION} --iteration ${PLUGIN_ITERATION} --epoch $PLUGIN_EPOCH \
     --prefix /usr/lib64/nagios/plugins \
     --description "Nagios Plugin - ${PLUGIN}" \
+    --url "${PLUGIN_URL}" \
     ${PLUGIN} &>/dev/null
   cp nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.i686.rpm packages/rhel/5/
   mv nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.i686.rpm packages/rhel/6/
 
-  # Build RHEL 5/6/7 x86_64 package
+  # Build RHEL 5/6/7/8/9 x86_64 package
   setarch x86_64 fpm -s dir -t rpm --architecture x86_64 \
     -n nagios-plugins-${PLUGIN_NAME_RHEL} \
     -v ${PLUGIN_VERSION} --iteration ${PLUGIN_ITERATION} --epoch $PLUGIN_EPOCH \
     --prefix /usr/lib64/nagios/plugins \
     --description "Nagios Plugin - ${PLUGIN}" \
+    --url "${PLUGIN_URL}" \
     ${PLUGIN} &>/dev/null
   cp nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.x86_64.rpm packages/rhel/5/
   cp nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.x86_64.rpm packages/rhel/6/
-  mv nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.x86_64.rpm packages/rhel/7/
+  cp nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.x86_64.rpm packages/rhel/7/
+  cp nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.x86_64.rpm packages/rhel/8/
+  mv nagios-plugins-${PLUGIN_NAME_RHEL}-${PLUGIN_VERSION}-${PLUGIN_ITERATION}.x86_64.rpm packages/rhel/9/
 done
 
-git rev-parse HEAD > last_success
+git rev-parse HEAD >last_success
